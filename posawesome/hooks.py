a86ca7d36bef0ffcca7731b140f1845b50597c36
Youssef Restom <youssefrestom@gmail.com>
2020-09-13 01:56:52 +0000

    feat: console from python

--- posawesome/hooks.py
+++ posawesome/hooks.py
@@ -17,6 +17,10 @@ app_license = "GPLv3"
 # include js, css files in header of desk.html
 # app_include_css = "/assets/posawesome/css/posawesome.css"
 # app_include_js = "/assets/posawesome/js/posawesome.js"
+app_include_js = [
+	"/assets/posawesome/node_modules/vuetify/dist/vuetify.js",
+	"/assets/posawesome/js/toConsole.min.js"
+	]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/posawesome/css/posawesome.css"
