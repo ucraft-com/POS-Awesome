21b82522de1cd54f6d42cf9fecd5eada7101b9e5
Youssef Restom <youssefrestom@gmail.com>
2020-09-15 14:53:48 +0000

    update items cards layout

--- posawesome/public/js/posapp/components/pos/ItemsCrads.vue
+++ posawesome/public/js/posapp/components/pos/ItemsCrads.vue
@@ -1,32 +1,71 @@
 <template>
-<v-card class="cards mx-auto grey lighten-5" style="max-height: 90vh; height: 90vh">
+<v-card style="max-height: 90vh; height: 90vh" class="cards my-0 py-0">
      <Customer></Customer>
-    <v-expansion-panels
-            hover="hover"
-            class="px-2"
-            >
-        <v-expansion-panel
-            v-for="project in projects"
-            :key="project.name"
-            >
-            <v-expansion-panel-header class="pl-2">{{ project.title }}</v-expansion-panel-header>
-            <v-expansion-panel-content class="px-1">
-                <v-row>
-                    <v-col cols="4">
-                        <div class="px-0 font-weight-bold">{{ project.person }}</div>
-                    </v-col>
-                    <v-col cols="4">
-                        <div class="px-0">{{ project.due }}</div>
-                    </v-col>
-                        <v-col cols="">
-                        <div class="px-0">{{ project.status }}</div>
-                    </v-col>
-                </v-row>
-                <div class="px-0 grey--text">{{ project.content }}</div>
-                
-            </v-expansion-panel-content>
-        </v-expansion-panel>
-    </v-expansion-panels>
+    <v-list class="my-0 py-0">
+      <v-list-item-group
+        v-model="selected"
+        color="primary"
+        class="my-0 py-0"
+      >
+        <template v-for="(item, index) in items">
+          <v-list-item :key="item.item_code" class="px-1">
+            <template v-slot:default="{ active, toggle }">
+                <v-list-item-action class="pa-0 mr-2 my-0">
+                    <v-btn icon color="red" @click="out_console(item.item_code)">
+                        <v-icon>mdi-delete</v-icon>
+                    </v-btn>
+                </v-list-item-action>
+              <v-list-item-content class="py-0">
+                    <v-row align="center"  class="ma-0">
+                        <v-col cols="4" class="pa-1">
+                             <div v-text="item.item_code"></div>
+                        </v-col>
+                        <v-col cols="1" class="pa-1">
+                            <v-btn icon small color="green">
+                                <v-icon>mdi-minus-box-outline</v-icon>
+                            </v-btn>
+                        </v-col>
+                        <v-col cols="2" class="pa-1 text-overline">
+                            <div align="center"  v-text="item.qty"></div>
+                        </v-col>
+                        <v-col cols="1" class="pa-1">
+                            <v-btn icon small  color="green">
+                                <v-icon>mdi-plus-box-outline</v-icon>
+                            </v-btn>
+                        </v-col>
+                        <v-col cols="2" class="pa-1 text-overline">
+                            <div align="center" v-text="item.price"></div>
+                        </v-col>
+                        <v-col cols="2" class="pa-1 text-overline">
+                            <div align="center" v-text="item.total"></div>
+                        </v-col>
+                    </v-row>
+              </v-list-item-content>
+              <v-list-item-action class=" py-0 my-0">
+                <v-icon
+                  v-if="!active"
+                  color="grey lighten-1"
+                >
+                  mdi-star
+                </v-icon>
+
+                <v-icon
+                  v-else
+                  color="yellow"
+                >
+                  mdi-star
+                </v-icon>
+              </v-list-item-action>
+            </template>
+          </v-list-item>
+
+          <v-divider class="my-0 py-0"
+            v-if="index + 1 < items.length"
+            :key="index"
+          ></v-divider>
+        </template>
+      </v-list-item-group>
+    </v-list>
 </v-card>
 </template>
 
@@ -35,12 +74,44 @@ import Customer from './Customer.vue'
 export default {
     data() {
         return {
-             projects: [
-                { title: 'Design 1 a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-                { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-                { title: 'Design 2 video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-                { title: 'Create a 3 community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-            ]
+            selected: 0,
+            items: [
+                {
+                    item_code: "Pepsi 200 ml",
+                    qty: '5',
+                    vat: '18',
+                    price: '3.6',
+                    total: '12,33',
+                },
+                {
+                    item_code: "Pepsi 330 ml Kutu Kola",
+                    qty: '2',
+                    vat: '18',
+                    price: '3',
+                    total: '6',
+                },
+                {
+                    item_code: "Favorite road trips",
+                    qty: '2',
+                    vat: '18',
+                    price: '3',
+                    total: '6',
+                },
+                {
+                    item_code: "Coca Cola Gazlı İçecek Kola 1.25 L",
+                    qty: '100',
+                    vat: '18',
+                    price: '123.99',
+                    total: '1231.23',
+                },
+                {
+                    item_code: "COCA COLA Coca Cola Kutu 200 Ml",
+                    qty: '2',
+                    vat: '18',
+                    price: '3',
+                    total: '6',
+                },
+            ],
         }
     },
     components: {
@@ -49,6 +120,9 @@ export default {
     methods: {
         sortBy(prop) {
             this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
+        },
+        out_console(value) {
+            console.log(value)
         }
     },
     mounted: function () {
