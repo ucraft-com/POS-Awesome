edb2062422f729a4ae76cefda67f1438163054f5
Youssef Restom <youssefrestom@gmail.com>
2021-04-16 18:26:01 +0000

    Version 1.1.0

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import frappe
-__version__ = '1.0.3'
+
+__version__ = "1.1.0"
 
 
 def console(*data):
-    frappe.publish_realtime('toconsole', data, user=frappe.session.user)
+    frappe.publish_realtime("toconsole", data, user=frappe.session.user)
