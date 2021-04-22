27f64594df23e50b0f7f188d4fa85c34b5e5ec1f
hcesar <hcesar@gmail.com>
2021-04-22 07:32:11 -0700

    f

--- posawesome/posawesome/api/posapp.py
+++ posawesome/posawesome/api/posapp.py
@@ -730,7 +730,7 @@ def search_invoices_for_return(invoice_name, company):
     invoices_list = frappe.get_list(
         "Sales Invoice",
         filters={
-            "name": ["like", f"%{invoice_name}%"],
+            "name": ["like", "%{invoice_name}%"],
             "company": company,
             "docstatus": 1,
         },
