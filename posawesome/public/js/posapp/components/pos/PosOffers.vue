b5e374900d225860dbaa5a25eb9214e22b044f58
Youssef Restom <youssefrestom@gmail.com>
2021-06-18 21:59:36 +0000

    fix: auto apply replace offer

--- posawesome/public/js/posapp/components/pos/PosOffers.vue
+++ posawesome/public/js/posapp/components/pos/PosOffers.vue
@@ -198,7 +198,7 @@ export default {
             }
           }
           if (newOffer.offer == 'Give Product' && !newOffer.give_item) {
-            newOffer.give_item = this.get_give_items(newOffer)[0];
+            newOffer.give_item = this.get_give_items(newOffer)[0].item_code;
           }
           this.pos_offers.push(newOffer);
           evntBus.$emit('show_mesage', {
