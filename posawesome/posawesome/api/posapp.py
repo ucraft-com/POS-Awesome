4df99c737e7fab777ede3fa375daa9c4b87e873d
Youssef Restom <youssefrestom@gmail.com>
2021-06-20 14:43:59 +0000

    feat: auto apply batchs for bundels

--- posawesome/posawesome/api/posapp.py
+++ posawesome/posawesome/api/posapp.py
@@ -19,9 +19,10 @@ from erpnext.accounts.doctype.sales_invoice.sales_invoice import get_bank_cash_a
 from erpnext.stock.get_item_details import get_item_details
 from erpnext.accounts.doctype.pos_profile.pos_profile import get_item_groups
 from frappe.utils.background_jobs import enqueue
+from erpnext.stock.doctype.batch.batch import get_batch_no, get_batch_qty
 import json
-from posawesome import console
 from posawesome.posawesome.api.posapp_customization import get_available_credit
+from posawesome import console
 
 
 @frappe.whitelist()
@@ -417,7 +418,7 @@ def submit_invoice(data):
         invoice_doc.is_pos = 0
 
     invoice_doc.payments = payments
-
+    set_batch_nos_for_bundels(invoice_doc, "warehouse", throw=True)
     invoice_doc.due_date = data.get("due_date")
     invoice_doc.flags.ignore_permissions = True
     frappe.flags.ignore_account_permission = True
@@ -458,6 +459,27 @@ def submit_invoice(data):
         )
 
     return {"name": invoice_doc.name, "status": invoice_doc.docstatus}
+
+
+def set_batch_nos_for_bundels(doc, warehouse_field, throw=False):
+    """Automatically select `batch_no` for outgoing items in item table"""
+    for d in doc.packed_items:
+        qty = d.get("stock_qty") or d.get("transfer_qty") or d.get("qty") or 0
+        has_batch_no = frappe.db.get_value("Item", d.item_code, "has_batch_no")
+        warehouse = d.get(warehouse_field, None)
+        if has_batch_no and warehouse and qty > 0:
+            if not d.batch_no:
+                d.batch_no = get_batch_no(
+                    d.item_code, warehouse, qty, throw, d.serial_no
+                )
+            else:
+                batch_qty = get_batch_qty(batch_no=d.batch_no, warehouse=warehouse)
+                if flt(batch_qty, d.precision("qty")) < flt(qty, d.precision("qty")):
+                    frappe.throw(
+                        _(
+                            "Row #{0}: The batch {1} has only {2} qty. Please select another batch which has {3} qty available or split the row into multiple rows, to deliver/issue from multiple batches"
+                        ).format(d.idx, d.batch_no, batch_qty, qty)
+                    )
 
 
 def redeeming_customer_credit(
