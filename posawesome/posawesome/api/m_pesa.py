825dc02e98e45a0952ff4e50064a8537fa514e06
Youssef <youssefrestom@gmail.com>
2021-11-10 23:57:54 +0000

    feat: confirmation & validation APIs & get_token

--- posawesome/posawesome/api/m_pesa.py
+++ posawesome/posawesome/api/m_pesa.py
@@ -2,18 +2,52 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+import frappe, requests
 from frappe import _
-import json
+from requests.auth import HTTPBasicAuth
+
+
+def get_token(app_key, app_secret, base_url):
+    authenticate_uri = "/oauth/v1/generate?grant_type=client_credentials"
+    authenticate_url = "{0}{1}".format(base_url, authenticate_uri)
+
+    r = requests.get(authenticate_url, auth=HTTPBasicAuth(app_key, app_secret))
+
+    return r.json()["access_token"]
 
 
 @frappe.whitelist(allow_guest=True)
-def confirmation(*args, **kwarg):
-    my_str = "args : " + str(args) + " - " + "kwarg : " + str(kwarg)
-    frappe.log_error(my_str)
+def confirmation(**kwargs):
+    try:
+        args = frappe._dict(kwargs)
+        doc = frappe.new_doc("Mpesa Payment Register")
+        doc.transactiontype = args.get("TransactionType")
+        doc.transid = args.get("TransID")
+        doc.transtime = args.get("TransTime")
+        doc.transamount = args.get("TransAmount")
+        doc.businessshortcode = args.get("BusinessShortCode")
+        doc.billrefnumber = args.get("BillRefNumber")
+        doc.invoicenumber = args.get("InvoiceNumber")
+        doc.orgaccountbalance = args.get("OrgAccountBalance")
+        doc.thirdpartytransid = args.get("ThirdPartyTransID")
+        doc.msisdn = args.get("MSISDN")
+        doc.firstname = args.get("FirstName")
+        doc.middlename = args.get("MiddleName")
+        doc.lastname = args.get("LastName")
+        doc.insert(ignore_permissions=True)
+        frappe.log_error("confirmation" + "  " + str(args), "confirmation")
+        frappe.db.commit()
+        context = {"ResultCode": 0, "ResultDesc": "Accepted"}
+        return dict(context)
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), str(e))
+        context = {"ResultCode": 1, "ResultDesc": "Rejected"}
+        return dict(context)
 
 
 @frappe.whitelist(allow_guest=True)
-def validation(*args, **kwarg):
+def validation(**kwargs):
+    args = frappe._dict(kwargs)
+    frappe.log_error("validation" + "  " + str(args), "validation")
     context = {"ResultCode": 0, "ResultDesc": "Accepted"}
     return dict(context)
