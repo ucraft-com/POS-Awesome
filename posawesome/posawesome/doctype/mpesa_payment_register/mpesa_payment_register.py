e353c5ef927eb8ef32dd84765f534444267fc6f9
Youssef <youssefrestom@gmail.com>
2021-11-16 12:38:51 +0000

    feat: create payment entry on submit of Mpesa Payment Register

--- posawesome/posawesome/doctype/mpesa_payment_register/mpesa_payment_register.py
+++ posawesome/posawesome/doctype/mpesa_payment_register/mpesa_payment_register.py
@@ -1,8 +1,32 @@
 # Copyright (c) 2021, Youssef Restom and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
+from posawesome.posawesome.api.payment_entry import create_payment_entry
+
 
 class MpesaPaymentRegister(Document):
-	pass
+    def before_submit(self):
+        if not self.transamount:
+            frappe.throw(_("Trans Amount is required"))
+        if not self.company:
+            frappe.throw(_("Company is required"))
+        if not self.customer:
+            frappe.throw(_("Customer is required"))
+        if not self.mode_of_payment:
+            frappe.throw(_("Mode of Payment is required"))
+        self.payment_entry = self.create_payment_entry()
+
+    def create_payment_entry(self):
+        payment_entry = create_payment_entry(
+            self.company,
+            self.customer,
+            self.transamount,
+            self.currency,
+            self.mode_of_payment,
+            self.posting_date,
+            self.transid,
+        )
+        return payment_entry.name
