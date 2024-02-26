e86ac8752da894567833d1b4fda9884748252c84
nihalroshan55 <nihalroshan55@gmail.com>
2024-02-26 16:24:40 +0530

    created the barcode

--- posawesome/hooks.py
+++ posawesome/hooks.py
@@ -94,7 +94,9 @@ doctype_js = {
 doc_events = {
     "Sales Invoice": {
         "validate": "posawesome.posawesome.api.invoice.validate",
-        "before_submit": "posawesome.posawesome.api.invoice.before_submit",
+        "before_submit": ["posawesome.posawesome.api.invoice.before_submit",
+                          "posawesome.posawesome.api.invoice.custom_generate_barcode",
+                          ],
         "before_cancel": "posawesome.posawesome.api.invoice.before_cancel",
     },
     "Customer": {
