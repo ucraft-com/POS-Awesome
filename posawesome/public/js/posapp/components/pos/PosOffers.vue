6a7179a2d281c3269b0d3c9e428a7670ba9aafba
kaviya.3111999@gmail.com <kaviya.3111999@gmail.com>
2024-04-10 21:19:01 +0530

    feat: remove offer indicator

--- posawesome/public/js/posapp/components/pos/PosOffers.vue
+++ posawesome/public/js/posapp/components/pos/PosOffers.vue
@@ -199,11 +199,6 @@ export default {
           if (newOffer.offer == 'Give Product' && !newOffer.give_item) {
             newOffer.give_item = this.get_give_items(newOffer)[0].item_code;
           }
-          this.pos_offers.push(newOffer);
-          evntBus.$emit('show_mesage', {
-            text: __('New Offer Available'),
-            color: 'warning',
-          });
         }
       });
     },
