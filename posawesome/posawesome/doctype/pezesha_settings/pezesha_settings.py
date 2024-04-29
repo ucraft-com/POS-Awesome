04d4837070f7caf6195a989221096eb29794690c
erpbox-sols <info@erpbox-solutions.com>
2024-04-29 15:54:21 +0300

    Update pezesha_settings.py

--- posawesome/posawesome/doctype/pezesha_settings/pezesha_settings.py
+++ posawesome/posawesome/doctype/pezesha_settings/pezesha_settings.py
@@ -34,7 +34,6 @@ class PezeshaSettings(Document):
 @frappe.whitelist()
 def pezesha_loan_offer(customer, pos_profile):
     pos = frappe.get_doc("POS Profile", pos_profile)
-    print("----------", pos.custom_pezesha_channel_id)
     pz_st = frappe.db.get_single_value('Pezesha Settings', 'authorization')
     url = 'https://api.pezesha.com/mfi/v1/borrowers/options'
     headers = {
@@ -62,14 +61,13 @@ def pezesha_loan_application(data, pos_profile):
 	res = json.loads(data)
 	pos = frappe.get_doc("POS Profile", pos_profile)
 	pz_st = frappe.db.get_single_value('Pezesha Settings', 'authorization')
-
-	url = 'https://api.pezesha.com/mfi/v1/borrowers/options'
+	url = 'https://api.pezesha.com/mfi/v1/borrowers/loans'
 	headers = {
 	    'Authorization': f'Bearer {pz_st}'
 	}
 	data = {
-		'channel': 'VICTORY_FARMS_KAWANGWARE',
-	    'identifier': 'VF-CUST-2023-10313',
+		'channel': pos.custom_pezesha_channel_id,
+	    'identifier': res.get('pezesha_customer_id'),
 	    'amount': res.get('amount'),
 	    'duration': res.get('duration'),
 	    'interest': res.get('interest'),
